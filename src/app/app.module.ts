a8602fa5e2e72f33b7840b4ee67908767cb377dd
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-09-26 15:37:38 +0300

    added the quote message,author and name class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteMessageComponent } from './quote-message/quote-message.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteMessageComponent,
+    
   ],
   imports: [
     BrowserModule,
