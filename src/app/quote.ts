e2a445b7d0ae4833c4eb0df01d0f33d0539ea83c
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-09-27 08:33:28 +0300

    adding the display functionalities on the form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     showMessage: boolean;
     showAuthor: boolean;
-    constructor(public quote_message: string,public Author: string,public name: string, public completeDate: Date){
+    constructor(public quote_message: string,public Author: string){
       this.showMessage=false;
       this.showAuthor=false;
     }
